082dfacb44e9710aa67230cd223b7fd55e1e9fe5
Kevin Finley and getro naissance <kevin+getro@>
2016-11-17 13:12:27 -0800

    add hash to this branch

--- lib/coin.rb
+++ lib/coin.rb
@@ -1,49 +1,31 @@
 class Fixnum
  define_method(:coin) do
+   numbers = {
+     "qaurters" => 0,
+     "dimes" => 0,
+     "nickels" => 0,
+     "pennies" => 0
+   }
    counter = self
-   until counter .==(0)
-     if self >= 25
-       number_q = self / 25
-        counter = self % 25
+   until counter == 0
+     if counter >= 25
+       number_q = counter / 25
+       numbers["qaurters"] = number_q
+        counter -= number_q * 25
      elsif counter >= 10
        number_d = counter / 10
-       counter = counter % 10
-
+       numbers["dimes"] = number_d
+        counter -= number_d * 10
      elsif counter >= 5
        number_n = counter / 5
-       counter = counter % 5
-     elsif counter <= 4
-       binding.pry
-       number_p = counter / 1
-       counter = counter % counter
+       numbers["nickels"] = number_n
+        counter -= number_n * 5
      else
-       break if counter == 0
+        number_p = counter
+       numbers["pennies"] = number_p
+        counter -= number_p
      end
    end
-  number_q.to_s() + " quarter, " + (number_d.to_s() + " dime, ") + (number_n.to_s() + "nickel, ") + (number_p.to_s() + " penny")
+  numbers
  end
 end
-
-
-
-# class Fixnum
-#  define_method(:coin) do
-#    counter = self
-#    until counter .==(0) do
-#      if self >= 25
-#        number_q = self / 25
-#         counter = self % 25
-#      elsif counter >= 10
-#        number_d = counter / 10
-#        counter = counter % 10
-#      elsif counter >= 5
-#        number_n = counter / 5
-#        counter = counter % 5
-#      elsif counter <= 4
-#        number_p = counter / 1
-#      end
-#      counter = counter % counter
-#    end
-#   number_q.to_s() + " quarter, " + (number_d.to_s() + " dime, ") + (number_n.to_s() + " nickel, ") + (number_p.to_s() + " penney, ")
-#  end
-# end
